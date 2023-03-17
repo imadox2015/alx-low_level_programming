98a09946d383eb28671b3a4c1aa3e8cf387787f1
imadox2015 <imadox2025@gmail.com>
2023-03-16 17:27:02 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
-*main - A program that prints a line with puts functio
-n
-Return: 0 (Success)
-*/
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzz
-le");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
