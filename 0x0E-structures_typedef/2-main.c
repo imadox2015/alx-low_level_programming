ff62722e6220a087ed3089ad43631903bc2dcb4a
imadox2015 <imadox2025@gmail.com>
2023-04-17 11:17:02 -0700

    Last commit

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -2,9 +2,9 @@
 #include "dog.h"
 
 /**
- * main - Entry point
+ * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -14,6 +14,5 @@ int main(void)
     my_dog.age = 3.5;
     my_dog.owner = "Bob";
     print_dog(&my_dog);
-
     return (0);
 }
